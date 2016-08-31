0732019bb594eda5378dd26caa2d2100d27000ed
Vincentius Madya <madya121@gmail.com>
2016-08-31 22:08:15 +0700

    Prime addition and Score build from multiplying second with maxBound

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -26,6 +26,7 @@ public class SceneManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Random.seed = (int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds;
         gameplayData = new GameplayData(1, 3);
 
         mainSlider.minValue = gameplayData.getLowerBound();
@@ -35,11 +36,11 @@ public class SceneManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.leftText.text = mainSlider.value.ToString();
-        this.rightText.text = gameplayData.getMiddle().ToString();
+        this.rightText.text = gameplayData.getTarget().ToString();
         this.scoreText.text = gameplayData.getScore().ToString();
 
 		// update slider indicator
-		if (mainSlider.value == gameplayData.getMiddle ()) {
+		if (mainSlider.value == gameplayData.getTarget ()) {
 			slideIndicator.color = colors[1];
 			buttonBG.color = colors[1];
 			buttonText.text = "HIT ME!";
@@ -59,7 +60,7 @@ public class SceneManager : MonoBehaviour {
 
     public void onWhozzClick() {    
         // Indicator is on the center
-        if (mainSlider.value == gameplayData.getMiddle()) {
+        if (mainSlider.value == gameplayData.getTarget()) {
 			scoreTextAnim.Animate ();
             nextLevel();
         }
@@ -70,12 +71,16 @@ public class SceneManager : MonoBehaviour {
     }
 
     private void nextLevel() {
-        gameplayData.addScore(gameplayData.getMiddle() * (long)gameplayData.getSecondLeft());
-        gameplayData.setBound(gameplayData.getLowerBound(), gameplayData.getUpperBound() + 10);
+        gameplayData.addScore(gameplayData.getUpperBound() * (long)gameplayData.getSecondLeft());
+        gameplayData.nextLevel();
 
         mainSlider.minValue = gameplayData.getLowerBound();
         mainSlider.maxValue = gameplayData.getUpperBound();
-        mainSlider.value = 1;
+        int pointer = (int)Random.Range(mainSlider.minValue, mainSlider.maxValue);
+        while (pointer == gameplayData.getTarget()) {
+            pointer = (int)Random.Range(mainSlider.minValue, mainSlider.maxValue);
+        }
+        mainSlider.value = pointer;
 
         gameplayData.addSecondLeft(2.0f);
     }
